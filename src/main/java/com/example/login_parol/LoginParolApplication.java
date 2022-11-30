26881cc8e32e5e77969a70b37949fb368a544140
Jaloliddin <zokirovjaloliddin0@gmail.com>
2022-11-30 10:59:25 +0500

    Initial commit

---
+++ src/main/java/com/example/login_parol/LoginParolApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_parol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginParolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginParolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/login_parol/LoginParolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_parol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginParolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
